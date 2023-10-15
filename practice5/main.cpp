b5385874dbde93074c9ddc7e8352ef1561daf554
Dayan Bravo <dayan3847@gmail.com>
2023-10-15 17:11:14 -0600

    import opencv and create video capture

--- practice5/main.cpp
+++ practice5/main.cpp
@@ -1,6 +1,15 @@
+#include "opencv2/opencv.hpp"
+#include "opencv2/highgui/highgui.hpp"
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char **argv) {
+    std::string filename = "./media/PelotaVerde.mkv";
+    cv::VideoCapture videoCapture = cv::VideoCapture(filename);
+    if (!videoCapture.isOpened()) {
+        std::cerr << "Error opening video file" << std::endl;
+        exit(1);
+    }
+
+    std::cout << "\033[1;32m" << "End" << "\033[0m" << std::endl;
     return 0;
 }
