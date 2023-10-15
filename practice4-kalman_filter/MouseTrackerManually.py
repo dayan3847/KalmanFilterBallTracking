930fedd7a823e106fdea85ce3b6477099e3a891d
Dayan Bravo <dayan3847@gmail.com>
2023-10-15 13:43:10 -0600

    test kalman filter

--- practice4-kalman_filter/MouseTrackerManually.py
+++ practice4-kalman_filter/MouseTrackerManually.py
@@ -3,7 +3,7 @@ import cv2
 import time
 
 
-class MouseTracker:
+class MouseTrackerManually:
 
     def __init__(self):
         self.window_name: str = 'Mouse Tracker'
@@ -18,45 +18,33 @@ class MouseTracker:
         cv2.namedWindow(self.window_name)
         cv2.setMouseCallback(self.window_name, self.mouse_callback)
 
-        # delta time in milliseconds and seconds.
-        # dtm: float = 10
-        # dts: float = dtm * 10e-3
+        # delta time
         self.dt: float = 1 / 60
-
-        # KalmanFilter
-        self.kalman = cv2.KalmanFilter(4, 2)
-
-        # Transition Matrix
-        self.kalman.transitionMatrix = np.array(
-            [
-                [1, 0, self.dt, 0],
-                [0, 1, 0, self.dt],
-                [0, 0, 1, 0],
-                [0, 0, 0, 1],
-            ],
-            dtype=np.float32)
-        # measurementMatrix
-        self.kalman.measurementMatrix = np.array([
+        # Transition Matrix (A)
+        self.A = np.array([
+            [1, 0, self.dt, 0],
+            [0, 1, 0, self.dt],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1],
+        ], dtype=np.float32)
+        # measurementMatrix (H)
+        self.H = np.array([
             [1, 0, 0, 0],
             [0, 1, 0, 0]
         ], dtype=np.float32)
-        # statePre
-        self.kalman.statePre = np.array(
-            [
-                [0],  # x
-                [0],  # y
-                [0],  # dx
-                [0],  # dy
-            ],
-            dtype=np.float32)
-        # errorCovPre
-        self.kalman.errorCovPre = np.eye(4, dtype=np.float32)
-        # processNoiseCov
-        self.kalman.processNoiseCov = np.identity(4, dtype=np.float32) * 1e-4
-        # measurementNoiseCov
-        self.kalman.measurementNoiseCov = np.identity(2, dtype=np.float32) * 10
-        # errorCovPost
-        self.kalman.errorCovPost = np.identity(4, dtype=np.float32) * .1
+        # state vector (x)
+        self.x = np.array([
+            [0],  # x
+            [0],  # y
+            [0],  # dx
+            [0],  # dy
+        ], dtype=np.float32)
+        # errorCovPre (P)
+        self.P = np.eye(4, dtype=np.float32)
+        # processNoiseCov (Q)
+        self.Q = np.identity(4, dtype=np.float32) * 1e-4
+        # measurementNoiseCov (R)
+        self.R = np.identity(2, dtype=np.float32) * 10
 
     def mouse_callback(self, event: int, x: int, y: int, flags: int, param) -> None:
         if event == cv2.EVENT_MOUSEMOVE:
@@ -66,21 +54,33 @@ class MouseTracker:
         self.running = True
         while self.running:
             # Predict
-            prediction: np.ndarray = self.kalman.predict()
+            self.x = np.dot(self.A, self.x)
+            self.P = np.dot(np.dot(self.A, self.P), self.A.T) + self.Q
             # Correct
-            self.kalman.correct(self.z)
+            # Get the z (z)
+            # Get the z error
+            y = self.z - np.dot(self.H, self.x)
+            # Get the z error covariance
+            S = np.dot(np.dot(self.H, self.P), self.H.T) + self.R
+            # Get the Kalman gain
+            K = np.dot(np.dot(self.P, self.H.T), np.linalg.inv(S))
+            # Update the state estimate
+            self.x = self.x + np.dot(K, y)
+            # Update the covariance
+            self.P = np.dot((np.eye(4, dtype=np.float32) - np.dot(K, self.H)), self.P)
+
             # Get the estimated position
-            estimated_position: np.ndarray = prediction.T[0][:2].astype(np.int32)
-            estimated_position_: np.ndarray = self.kalman.statePost.T[0][:2].astype(np.int32)
+            estimated_position: np.ndarray = self.x.T[0][:2].astype(np.int32)
 
             self.points = np.append(self.points, [self.z.astype(np.int32)], axis=0)
             self.points_kalman = np.append(self.points_kalman, [estimated_position], axis=0)
 
             # Draw
             if not np.array_equal(self.points[-2], self.points[-1]):
-                cv2.line(self.img, self.points[-2], self.points[-1], (0, 0, 255), 1, cv2.LINE_AA, 0)
+                cv2.line(self.img, tuple(self.points[-2]), tuple(self.points[-1]), (0, 0, 255), 1, cv2.LINE_AA, 0)
             if not np.array_equal(self.points_kalman[-2], self.points_kalman[-1]):
-                cv2.line(self.img, self.points_kalman[-2], self.points_kalman[-1], (255, 0, 0), 1, cv2.LINE_AA, 0)
+                cv2.line(self.img, tuple(self.points_kalman[-2]), tuple(self.points_kalman[-1]), (255, 0, 0), 1,
+                         cv2.LINE_AA, 0)
 
             # Show image
             cv2.imshow(self.window_name, self.img)
@@ -93,5 +93,5 @@ class MouseTracker:
 
 
 if __name__ == '__main__':
-    mt: MouseTracker = MouseTracker()
+    mt: MouseTrackerManually = MouseTrackerManually()
     mt.run()
