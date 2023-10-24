894448cdf4c9d28a7b507e7bf8ed275f64638255
Dayan Bravo <dayan3847@gmail.com>
2023-10-24 00:03:02 -0600

    feat: added submodules git repo by arturo

--- practice5/SegColorLab.cpp
+++ practice5/SegColorLab.cpp
@@ -1,7 +1,9 @@
-#include "opencv2/opencv.hpp"
 #include <iostream>
+#include <chrono>
+#include <thread>
+#include "opencv2/opencv.hpp"
 #include "lib/arturo/functions.h"
-#include "lib/arturo/Circle.h"
+#include "lib/arturo/Circle/Circle.h"
 #include "lib/tools.h"
 
 
@@ -33,23 +35,33 @@ int main(int argc, char** argv)
 	cv::namedWindow(inputWinName, 1);
 	cv::namedWindow(maskWinName, 1);
 
-	int dSlidePos = 16;
+	int sleep = 1;
+	cv::createTrackbar("Sleep", inputWinName, &sleep, 1);
+	// Slide 1 (distancia)
+	int dSlidePos = 211;
 	arturo::barData umDist(40. / SLIDE_MAX, 10);
 	cv::createTrackbar("umDist", inputWinName, &dSlidePos, SLIDE_MAX, arturo::umDistChange,
 		(void*)&umDist);
 	arturo::umDistChange(SLIDE_MAX, (void*)&umDist);
-
+	// Slide 2 (luz)
 	int lSlidePos = 16;
 	arturo::barData umLuz(100. / SLIDE_MAX, 0);
 	cv::createTrackbar("umLuz", inputWinName, &lSlidePos, SLIDE_MAX, arturo::umLuzChange,
 		(void*)&umLuz);
 	arturo::umLuzChange(0, (void*)&umLuz);
-
+	// Slide 3 (error permitido)
+	int error = 1;
+	cv::createTrackbar("Error", inputWinName, &error, 100);
+	// Slide 3 (error permitido)
+	int contourPointMinCount = 50;
+	cv::createTrackbar("Contour Point Count Min", inputWinName, &contourPointMinCount, 100);
 
 	cv::Mat mask, mean, iCov;
 	bool first = true;
+	int frameCount = 0;
 	do
 	{
+		std::cout << "\033[1;32m" << "Frame: " << frameCount++ << "\033[0m" << std::endl;
 		// We capture an image, and validate that the operation worked.
 		inputVideoCapture >> inputFrame;
 		if (inputFrame.empty())
@@ -87,30 +99,35 @@ int main(int argc, char** argv)
 		}
 
 		// Fit Circles
-		std::vector<arturo::Circle> circles;
+		std::vector<Circle> circles;
 		for (auto& contour : contours)
 		{
-			std::vector<arturo::Point3s> pts;
+			if (contourPointMinCount > contour.size())
+				continue;
+			std::cout << "cantidad de puntos del contorno: " << contour.size() << std::endl;
+			std::vector</*arturo::*/Point3s> pts;
 			for (auto& contourPoint : contour)
 			{
-				arturo::Point3s p;
+				/*arturo::*/Point3s p;
 				p.x = contourPoint.x;
 				p.y = contourPoint.y;
 				p.z = 0;
 				pts.push_back(p);
 			}
 			// Ransac Fit
-			arturo::Circle c(pts);
+			/*arturo::*/Circle c(pts);
 			unsigned int nInl;
 			float w = .6;
 			float sigma = 1;
 			float p = .99;
 			float e = c.ransacFit(pts, nInl, w, sigma, p);
+			if (0 > e)
+				continue;
+			if ((float)error < e)
+				continue;
 			std::cout << "e: " << e << " r: " << c.r << std::endl;
-			if (0 > e or 5 < e)
-				continue;
-			if (20 > c.r)
-				continue;
+//			if (20 > c.r)
+//				continue;
 			circles.push_back(c);
 		}
 
@@ -119,7 +136,7 @@ int main(int argc, char** argv)
 		{
 			cv::Point center(cvRound(c.h), cvRound(c.k));
 			int radius = cvRound(c.r);
-			if (0 >= radius || 1000 < radius)
+			if (0 >= radius)
 				continue;
 			cv::circle(inputFrame, center, radius, cv::Scalar(0, 0, 255),
 				3, 8, 0);
@@ -128,6 +145,11 @@ int main(int argc, char** argv)
 		// Show images
 		imshow(inputWinName, inputFrame);
 		imshow(maskWinName, mask);
+
+		std::cout << "error permitido: " << error << std::endl;
+
+		if (0 < sleep)
+			std::this_thread::sleep_for(std::chrono::seconds(1));
 
 		// If the user presses a key, the cycle ends.
 	} while (cv::waitKeyEx(30) < 0);
