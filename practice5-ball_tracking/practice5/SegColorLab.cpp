d1afcb22bcde2793e36fb9bacbc5781f12d8b7c3
Dayan Bravo <dayan3847@gmail.com>
2023-11-07 03:21:32 -0600

    feat: predict and correct ok

--- practice5-ball_tracking/practice5/SegColorLab.cpp
+++ practice5-ball_tracking/practice5/SegColorLab.cpp
@@ -32,11 +32,11 @@ int main(int argc, char** argv)
 	int sleep = 1;
 	cv::createTrackbar("Sleep", inputWinName, &sleep, 1);
 	// Slide 1 (distancia)
-	int dSlidePos = 211;
-	arturo::barData umDist(40. / SLIDE_MAX, 10);
-	cv::createTrackbar("umDist", inputWinName, &dSlidePos, SLIDE_MAX, arturo::umDistChange,
-		(void*)&umDist);
-	arturo::umDistChange(SLIDE_MAX, (void*)&umDist);
+	int dSlidePos = 200;
+//	arturo::barData umDist(40. / SLIDE_MAX, 47);
+//	cv::createTrackbar("umDist", inputWinName, &dSlidePos, SLIDE_MAX, arturo::umDistChange,
+//		(void*)&umDist);
+//	arturo::umDistChange(SLIDE_MAX, (void*)&umDist);
 	// Slide 2 (luz)
 	int lSlidePos = 16;
 	arturo::barData umLuz(100. / SLIDE_MAX, 0);
@@ -44,7 +44,7 @@ int main(int argc, char** argv)
 		(void*)&umLuz);
 	arturo::umLuzChange(0, (void*)&umLuz);
 	// Slide 3 (error permitido)
-	int error = 1;
+	int error = 5;
 	cv::createTrackbar("Error", inputWinName, &error, 100);
 	// Slide 3 (error permitido)
 	int contourPointMinCount = 50;
@@ -52,6 +52,8 @@ int main(int argc, char** argv)
 
 	cv::Mat inputFrame, inputFrameLab, mask, mean, iCov;
 	int dt = 0;
+	// Crear un vector vacio de Z (mediciones)
+	std::vector<cv::Mat> list_Z;
 
 	// KalmanFilter
 	cv::KalmanFilter kalmanFilter(6, 5, 0);
@@ -59,11 +61,10 @@ int main(int argc, char** argv)
 	setIdentity(kalmanFilter.measurementNoiseCov, Scalar::all(10));
 	setIdentity(kalmanFilter.errorCovPost, Scalar::all(.1));
 
-	Mat prediction;
 	int frameCount = -1;
 	do
 	{
-		std::cout << "\033[1;32m" << "Frame: " << frameCount++ << "\033[0m" << std::endl;
+		std::cout << "\033[1;32m" << "Frame: " << ++frameCount << "\033[0m" << std::endl;
 		// We capture an image, and validate that the operation worked.
 		inputVideoCapture >> inputFrame;
 		if (inputFrame.empty())
@@ -85,7 +86,7 @@ int main(int argc, char** argv)
 				0, 0, 0, 0, 0, 1
 			);
 
-		// Matrix H
+//		// Matrix H
 //		kalmanFilter.measurementMatrix =
 
 
@@ -100,48 +101,75 @@ int main(int argc, char** argv)
 
 			cv::Size sz(inputFrame.cols, inputFrame.rows);
 			mask = cv::Mat::ones(sz, CV_8U);
-
-			kalmanFilter.statePre = (Mat_<float>(6, 1)
-				<<
-				1,
-				1,
-				1,
-				0,
-				0,
-				0
-			);
 		}
 		else
 		{
 			arturo::convertLab(inputFrame, inputFrameLab);
 		}
-		prediction = kalmanFilter.predict();
+		Mat prediction = kalmanFilter.predict();
 
 		Circle* c = nullptr;
 		cv::Mat Z;
+//		std::cout << "\033[1;32m" << "umDistVal: " << umDist.val << "\033[0m" << std::endl;
+//		std::cout << "\033[1;32m" << "umLuz.val: " << umLuz.val << "\033[0m" << std::endl;
 		dayan::makeMeasurement(
 			inputFrameLab,
 			mask,
 			mean,
 			iCov,
-			umDist.val,
+			50,  //umDist.val,
 			umLuz.val,
 			contourPointMinCount,
 			error,
 			c,
-			Z
+			list_Z,
+			Z,
+			dt
 		);
 		if (nullptr == c)
 		{
-			std::cout << "No se encontro el circulo" << std::endl;
+			std::cout << "\033[1;31m" << "No se encontro el circulo" << "\033[0m" << std::endl;
 			continue;
 		}
-		kalmanFilter.correct(Z);
+		dayan::printMat(Z, "Z");
+		//dayan::drawCircle(inputFrame, c);
+		dayan::drawCircleByZ(inputFrame, Z, cv::Scalar(255, 0, 0));
 
-		cv::Point center(cvRound(c->h), cvRound(c->k));
-		int radius = cvRound(c->r);
-		cv::circle(inputFrame, center, radius, cv::Scalar(0, 0, 255),
-			3, 8, 0);
+		if (0 == frameCount)
+		{
+			dayan::getX(Z, kalmanFilter.statePre);
+			kalmanFilter.statePost = kalmanFilter.statePre;
+			dayan::printMat(kalmanFilter.statePre, "X");
+		}
+		else
+		{
+			cv::Mat h;
+			dayan::getH(kalmanFilter.statePre, h, kalmanFilter.measurementMatrix);
+
+			//cv::Mat corrected = kalmanFilter.correct(Z);
+			Mat HxQxHt = kalmanFilter.measurementMatrix * kalmanFilter.errorCovPre * kalmanFilter.measurementMatrix.t()
+				+ kalmanFilter.measurementNoiseCov;
+			Mat HxQxHt_inv;
+			cv::invert(HxQxHt, HxQxHt_inv, cv::DECOMP_LU);
+			kalmanFilter.gain = kalmanFilter.errorCovPre * kalmanFilter.measurementMatrix.t() * HxQxHt_inv;
+			kalmanFilter.statePost = kalmanFilter.statePre + kalmanFilter.gain * (Z - h);
+			kalmanFilter.errorCovPost =
+				(cv::Mat::eye(6, 6, CV_32F) - kalmanFilter.gain * kalmanFilter.measurementMatrix)
+					* kalmanFilter.errorCovPre;
+
+			dayan::printMat(kalmanFilter.statePost, "corrected");
+			// corrected color green
+			dayan::drawCircleByX(inputFrame, kalmanFilter.statePost, cv::Scalar(0, 255, 0));
+		}
+		//cv::Mat predicted = kalmanFilter.predict();
+		kalmanFilter.statePre = kalmanFilter.transitionMatrix * kalmanFilter.statePost;
+		kalmanFilter.errorCovPre =
+			kalmanFilter.transitionMatrix * kalmanFilter.errorCovPost * kalmanFilter.transitionMatrix.t()
+				+ kalmanFilter.processNoiseCov;
+
+		dayan::printMat(kalmanFilter.statePre, "predicted");
+		// predicted color red
+		dayan::drawCircleByX(inputFrame, kalmanFilter.statePre, cv::Scalar(0, 0, 255));
 
 		// Show images
 		imshow(inputWinName, inputFrame);
@@ -154,6 +182,7 @@ int main(int argc, char** argv)
 
 		dt = 0;
 		// If the user presses a key, the cycle ends.
+//		break;
 	} while (cv::waitKeyEx(30) < 0);
 
 	imwrite("./media/LastFrame.png", inputFrame);
--- practice5-ball_tracking/practice5/lib/tools.h
+++ practice5-ball_tracking/practice5/lib/tools.h
@@ -14,6 +14,25 @@
 
 namespace dayan
 {
+
+	// Matriz de calibracion
+	cv::Mat k = (cv::Mat_<float>(3, 3)
+		<<
+		1377.8036814997304, 0, 400.02681782947193,
+		0, 1377.8036814997304, 300.096061319675721,
+		0, 0, 1
+	);
+
+	// Matriz de calibracion (inversa)
+	cv::Mat kInv = (cv::Mat_<float>(3, 3)
+		<<
+		0.000725792805918116, 0, -0.290336586554948,
+		0, 0.000725792805918116, -0.217807562390183,
+		0, 0, 1
+	);
+
+	float realR = .03;
+
 	bool isInteger(const std::string& s)
 	{
 		try
@@ -155,7 +174,9 @@ namespace dayan
 		int const& contourPointMinCount,
 		int const& error,
 		Circle*& circle,
-		cv::Mat& Z
+		std::vector<cv::Mat>& list_Z,
+		cv::Mat& Z,
+		int const& dt
 	)
 	{
 		makeMeasurement(
@@ -172,20 +193,6 @@ namespace dayan
 		if (nullptr == circle)
 			return;
 
-		// Matriz de calibracion
-//		cv::Mat k = (cv::Mat_<float>(3, 3)
-//			<<
-//			1377.8036814997304, 0, 400.02681782947193,
-//			0, 1377.8036814997304, 300.096061319675721,
-//			0, 0, 1
-//		);
-		// Matriz de calibracion (inversa)
-		cv::Mat kInv = (cv::Mat_<float>(3, 3)
-			<<
-			0.000725792805918116, 0, -0.290336586554948,
-			0, 0.000725792805918116, -0.217807562390183,
-			0, 0, 1
-		);
 		// Centro en pixeles
 		cv::Mat centerPx = (cv::Mat_<float>(3, 1)
 			<<
@@ -220,12 +227,163 @@ namespace dayan
 //		cv::Mat p2Me = kInv * p2Px;
 //		float radiusMeTest3 = p2Me.at<float>(1) - centerMe.at<float>(1);
 
-			Z = (cv::Mat_<float>(3, 1)
+
+		cv::Mat prevZ = list_Z.empty() ? cv::Mat::zeros(5, 1, CV_32F) : list_Z.back();
+		float xp = 0 == dt ? 0 : (centerMe.at<float>(0) - prevZ.at<float>(0)) / (float)dt;
+		float yp = 0 == dt ? 0 : (centerMe.at<float>(1) - prevZ.at<float>(1)) / (float)dt;
+
+		Z = (cv::Mat_<float>(5, 1)
 			<<
 			centerMe.at<float>(0),
 			centerMe.at<float>(1),
+			xp,
+			yp,
 			radiusMe
 		);
+		list_Z.push_back(Z);
+	}
+
+	void drawCircle(const cv::Mat& inputFrame, Circle*& c)
+	{
+		cv::Point center(cvRound(c->h), cvRound(c->k));
+		int radius = cvRound(c->r);
+		cv::circle(inputFrame, center, radius, cv::Scalar(0, 0, 255),
+			3, 8, 0);
+	}
+
+	// obtener un estado inicial a partir de una medicion
+	void getX(const cv::Mat& ZZ, cv::Mat& XX)
+	{
+		auto x = ZZ.at<float>(0);
+		auto y = ZZ.at<float>(1);
+		auto r = ZZ.at<float>(4);
+
+		auto Z = realR / r;
+		auto X = x * Z;
+		auto Y = y * Z;
+
+		XX = (cv::Mat_<float>(6, 1)
+			<<
+			X,
+			Y,
+			Z,
+			0,
+			0,
+			0
+		);
+	}
+	// obtener medicion a partir del estado
+	void getZ(const cv::Mat& X, const cv::Mat& H, cv::Mat& Z)
+	{
+		Z = H * X;
+	}
+	// a partir de un estado, obtener h(X) y su jacobiano
+	void getH(const cv::Mat& XX, cv::Mat& h, cv::Mat& H)
+	{
+		float X = XX.at<float>(0);
+		float Y = XX.at<float>(1);
+		float Z = XX.at<float>(2);
+		float Xp = XX.at<float>(3);
+		float Yp = XX.at<float>(4);
+		float Zp = XX.at<float>(5);
+
+		h = (cv::Mat_<float>(5, 1)
+			<<
+			X / Z,
+			Y / Z,
+			(Xp + (X / Z) * Zp) / Z,
+			(Yp + (Y / Z) * Zp) / Z,
+			realR / Z
+		);
+		auto Z2 = Z * Z;
+		auto Z3 = Z2 * Z;
+		auto Zp_Z2 = Zp / Z2;
+		H = (cv::Mat_<float>(5, 6)
+			<<
+			1 / Z, 0, -X / Z2, 0, 0, 0,
+			0, 1 / Z, -Y / Z2, 0, 0, 0,
+			Zp_Z2, 0, Xp / Z2 - 2 * (X * Zp + Z * Xp) / Z3, 1 / Z, 0, X / Z2,
+			0, Zp_Z2, Yp / Z2 - 2 * (Y * Zp + Z * Yp) / Z3, 0, 1 / Z, Y / Z2,
+			0, 0, -realR / Z2, 0, 0, 0
+		);
+	}
+
+	// pintar el circulo a partir de una medicion
+	void drawCircleByZ(
+		const cv::Mat& inputFrame,
+		const cv::Mat& ZZ,
+		const cv::Scalar& color
+	)
+	{
+		// Centro en metros
+		cv::Mat centerMe = (cv::Mat_<float>(3, 1)
+			<<
+			ZZ.at<float>(0),
+			ZZ.at<float>(1),
+			1
+		);
+		// Centro en pixeles
+		cv::Mat centerPx = k * centerMe;
+		centerPx = centerPx / centerPx.at<float>(2);
+		cv::Point center(cvRound(centerPx.at<float>(0)), cvRound(centerPx.at<float>(1)));
+		// Radio en metros
+		float radiusMe = ZZ.at<float>(4);
+		// Radio en pixeles
+		float radiusPx = radiusMe * k.at<float>(0, 0);
+		int radius = cvRound(radiusPx);
+		if (radius > 0)
+			cv::circle(inputFrame, center, radius, color,
+				3, 8, 0);
+	}
+
+	// pintar el circulo a partir de un estado
+	void drawCircleByX(
+		const cv::Mat& inputFrame,
+		const cv::Mat& XX,
+		const cv::Scalar& color
+	)
+	{
+		float X = XX.at<float>(0);
+		float Y = XX.at<float>(1);
+		float Z = XX.at<float>(2);
+
+		cv::Mat ZZ = (cv::Mat_<float>(5, 1)
+			<<
+			X / Z,
+			Y / Z,
+			0,
+			0,
+			realR / Z
+		);
+
+		drawCircleByZ(inputFrame, ZZ, color);
+	}
+
+	std::string matToString(const cv::Mat& mat, bool tab = false)
+	{
+		char s = tab ? '\t' : ' ';
+		std::ostringstream stream;
+		std::string matString;
+		for (int i = 0; i < mat.rows; i++)
+		{
+			for (int j = 0; j < mat.cols; j++)
+			{
+				stream << mat.at<double>(i, j);
+				matString += stream.str();
+				if (j < mat.cols - 1)
+					matString += s;
+				stream.str("");
+			}
+			matString += "\n";
+		}
+		return matString;
+	}
+
+	void printMat(const cv::Mat& mat, const std::string& name = "Mat:")
+	{
+		std::cout << name << std::endl;
+		std::string matString = matToString(mat, true);
+		std::cout << matString << std::endl;
 	}
 }
 
