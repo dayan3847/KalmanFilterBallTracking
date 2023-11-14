479ff7c90db69d29ce4da5eefeffeb3fb5be0dbd
Dayan Bravo <dayan3847@gmail.com>
2023-11-14 14:10:01 -0600

    feat: trabajo en el local de arturo

--- practice5-ball_tracking/practice5/lib/dayan/KalmanFilterExtendedImplicit.h
+++ practice5-ball_tracking/practice5/lib/dayan/KalmanFilterExtendedImplicit.h
@@ -6,13 +6,34 @@
 #define KALMANFILTEREXTENDEDIMPLICIT_H
 
 #include "KalmanFilterExtended.h"
+#include "functions.h"
 
 namespace dayan
 {
-	// Extended Kalman Filter Implicit
+	// Implicit Extended Kalman Filter
 	class KalmanFilterExtendedImplicit : public dayan::KalmanFilterExtended
 	{
+	public:
+		cv::Mat J; //!< Jacobian matrix h respecto a Z
 
+		void correct(const cv::Mat& Z) override
+		{
+//			dayan::getJ(Xp, J);
+			R = J * R * J.t();
+
+
+			cv::Mat h;
+			dayan::getH(Xp, h, H);
+			cv::Mat Ht = H.t();
+
+			cv::Mat HxPpxHtmR = H * Pp * Ht + R;
+			cv::Mat inv_HxPpxHtmR;
+			cv::invert(HxPpxHtmR, inv_HxPpxHtmR, cv::DECOMP_LU);
+			K = Pp * Ht * inv_HxPpxHtmR;
+			X = Xp + K * (Z - h);
+			cv::Mat I = cv::Mat::eye(6, 6, CV_32F);
+			P = (I - K * H) * Pp;
+		}
 	};
 } // dayan
 
